2cac2d06021763e5cd3ed3823142e41cbc464c6a
Thomas Madden <madde120@mail.chapman.edu>
2017-12-18 11:17:20 -0800

    Big Data Federation - Gold and Silver

--- TommyMadden/gold.php
+++ TommyMadden/gold.php
@@ -7,11 +7,8 @@
  */
 
 $url = 'https://www.investing.com/commodities/gold-historical-data';
-$headers = @get_headers($url);
 
-/*if(strpos($headers[0],'200')===false) {
-    header('Location: index.php');
-}*/
+// Get HTML from gold page
 
 $ch = curl_init();
 $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
@@ -21,32 +18,46 @@ curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 $data = curl_exec($ch);
 curl_close($ch);
 
+// Divide HTML code up by line breaks into an array
 $pieces = explode("\n", $data);
 
-$gold_prices;
+// Initial values
+$gold_prices = null;
 $recordPrices = false;
 $current_date = 0;
 $current_value = 1;
+
+// Loop through array of lines of code
 for ($i = 0; $i < sizeof($pieces); ++$i)
 {
+    // If the 'results_box' div is in the present line of code, begin recording prices
     if (strpos($pieces[$i], '<div id="results_box">') !== false)
     {
         $recordPrices = true;
     }
+
+    // If recording prices and the line of HTML code contains ', 2017' it must have a date
     if ($recordPrices && strpos($pieces[$i], ', 2017') !== false)
     {
+        // Parse through the line to find the date, add as the first element of the row
         $gold_prices[$current_date][0] = substr($pieces[$i], strpos($pieces[$i], '">') + 2, strpos($pieces[$i], '</td>') - strpos($pieces[$i], '">') - 2);
     }
+
+    // If recording prices and the line of HTML code contains a value, pick it up and keep going
     else if ($recordPrices && strpos($pieces[$i], 'data-real-value=') !== false)
     {
         $gold_prices[$current_date][$current_value] = substr($pieces[$i], strpos($pieces[$i], '">') + 2, strpos($pieces[$i], '</td>')- strpos($pieces[$i], '">') - 2);
         $current_value++;
     }
+
+    // If recording prices and the line of HTML code contains the end of a table row, that date is completed
     if ($recordPrices && strpos($pieces[$i], '</tr>') !== false)
     {
         $current_date++;
         $current_value = 1;
     }
+
+    // If recording prices and the line of HTML code contains the end of a table body, the data is finished
     if ($recordPrices && strpos($pieces[$i], '</tbody>') !== false)
     {
         $recordPrices = false;
@@ -54,31 +65,43 @@ for ($i = 0; $i < sizeof($pieces); ++$i)
 }
 
 ?>
-
+<!DOCTYPE html>
 <html>
 <head>
     <title>Historical Prices of Gold</title>
     <link rel="stylesheet" type="text/css" href="css/gold_and_silver.css" />
+    <link href="https://fonts.googleapis.com/css?family=Arvo|Roboto|Ubuntu" rel="stylesheet">
 </head>
 <body>
 <div id="content_container">
     <div id="content">
-        <h1 id="title">Historical Prices of Gold</h1>
-        <div id="data">
+        <h1 id="title">Historical Prices of Gold</h1><br>
+        <table id="data">
+            <tr>
+                <td>Date</td><td>Price</td><td>Open</td><td>High</td><td>Low</td><td>Vol.</td>
+            </tr>
+            <tr>
+                <td colspan="6" style="font-size: 3px"></td>
+            </tr>
             <?php
-            echo "<br>Date\t\t\tPrice\t\tOpen\t\tHigh\t\tLow\t\t\tVol.";
+            // For each date
             for ($i = 0; $i < sizeof($gold_prices); ++$i) {
-                for ($j = 0; $j < sizeof($gold_prices[$i]); ++$j) {
-                    echo  trim($gold_prices[$i][$j]) . "\t" . "\t";
-                }
-                echo "\n";
-            }
 
+                // Make a table row
+                echo "<tr>\n\t";
+
+                // Loop through the values
+                for ($j = 0; $j < sizeof($gold_prices[$i]); ++$j) {
+                    echo "<td>" . trim($gold_prices[$i][$j]) . "</td>\n\t";       // Display each value
+                }
+                echo "</tr>\n";
+            }
             ?>
-</div>
+        </table>
+    </div>
 </div>
 <div id="form_container">
-    <h1>Date Range Calculation:</h1><br>
+    <h1 id="title">Date Range Calculation:</h1><br>
     <form action="date_range.php" method="GET" id="date_range_form">
         Date Start: <input type="text" name="date-start" /><br><br>
         Date End: <input type="text" name="date-end" /><br><br>
